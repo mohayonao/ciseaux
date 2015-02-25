c74fdfe57839e024365645356244d44fc383069e
mohayonao <mohayonao@gmail.com>
2015-02-25 23:43:31 +0900

    add InlineWorker

---
+++ test/inline-worker.js
@@ -0,0 +1,25 @@
+"use strict";
+
+import assert from "power-assert";
+import InlineWorker from "../src/inline-worker";
+
+describe("InlineWorker", () => {
+  describe("constructor(func: function, self: object)", () => {
+    it("works", (done) => {
+      let self = {};
+      let worker = new InlineWorker(function() {
+        self.onmessage = function(e) {
+          assert(e.data === "hello");
+          self.postMessage("good bye");
+        };
+      }, self);
+
+      worker.onmessage = (e) => {
+        assert(e.data === "good bye");
+        done();
+      };
+
+      worker.postMessage("hello");
+    });
+  });
+});
