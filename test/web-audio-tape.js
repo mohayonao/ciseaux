7cdccf5e10754dbd59acc8e369fc1cfae25da834
mohayonao <mohayonao@gmail.com>
2015-02-27 17:28:33 +0900

    add WebAudioTape

---
+++ test/web-audio-tape.js
@@ -0,0 +1,63 @@
+"use strict";
+
+import assert from "power-assert";
+import sinon from "sinon";
+import renderer from "../src/renderer";
+import Tape from "../src/tape";
+import WebAudioTape from "../src/web-audio-tape";
+
+describe("WebAudioTape", () => {
+  let audioContext;
+
+  before(() => {
+    audioContext = new global.AudioContext();
+  });
+
+  describe("constructor(audioBuffer: AudioBuffer)", () => {
+    it("works", () => {
+      sinon.spy(renderer, "transfer");
+
+      let buffer = audioContext.createBuffer(2, 100, 44100);
+      let tape = new WebAudioTape(buffer);
+
+      assert(tape instanceof WebAudioTape);
+      assert(tape instanceof Tape);
+      assert(renderer.transfer.calledOnce);
+
+      renderer.transfer.restore();
+    });
+  });
+  describe("#render(audioContext: AudioContext, numberOfChannels: number): Promise<AudioBuffer>", () => {
+    it("works", () => {
+      let buffer = audioContext.createBuffer(2, 100, 44100);
+
+      buffer.getChannelData(0).set([ 0, 1, 2, 3, 4 ]);
+      buffer.getChannelData(1).set([ 5, 6, 7, 8, 9 ]);
+
+      let tape = new WebAudioTape(buffer);
+
+      return tape.render(audioContext).then((audioBuffer) => {
+        assert(audioBuffer !== buffer);
+        assert(audioBuffer instanceof global.AudioBuffer);
+        assert.deepEqual(audioBuffer.getChannelData(0), buffer.getChannelData(0));
+        assert.deepEqual(audioBuffer.getChannelData(1), buffer.getChannelData(1));
+      }, () => {
+        throw new Error("NOT REACHED");
+      });
+    });
+  });
+  describe("#dispose(): void", () => {
+    it("works", () => {
+      sinon.spy(renderer, "dispose");
+
+      let buffer = audioContext.createBuffer(2, 100, 44100);
+      let tape = new WebAudioTape(buffer);
+
+      tape.dispose();
+
+      assert(renderer.dispose.calledOnce);
+
+      renderer.dispose.restore();
+    });
+  });
+});
