b8dc63140cc7da645914004600daafeb165f7eae
mohayonao <mohayonao@gmail.com>
2015-12-21 23:35:47 +0900

    replace ouroboros-worker -> inline-worker

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,7 @@
-const OUroborosWorker = require("ouroboros-worker");
+const InlineWorker = require("inline-worker");
 const render = require("./render-worker");
 
-let worker = new OUroborosWorker(render.self);
+let worker = new InlineWorker(render, render.self);
 let __callbacks = [];
 let __data = 1; // data 0 is reserved for silence
 
